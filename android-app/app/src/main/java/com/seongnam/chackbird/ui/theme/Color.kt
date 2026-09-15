45a5b4a6d39b4c32cdd4731880009017cd4b8600
TuttoShin <starprickle29@gmail.com>
2026-09-16 00:54:09 +0900

    chore: 안드로이드 앱 패키지 구조 초기 설계

---
+++ android-app/app/src/main/java/com/seongnam/chackbird/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.seongnam.chackbird.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// 착새 캐릭터 메인 컬러
+val ChackBlue = Color(0xFF224E9D)
+
+val ChackSkyBlue = Color(0xFF67A1EB)
+
+val ChackYellow = Color(0xFFF7CE46)
+
+val ChackPink = Color(0xFFFFB3B3)
+
+val ChackBackground = Color(0xFFEAF2F8)
+
+val ChackText = Color(0xFF1E2024)
